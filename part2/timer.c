61286a3257a01ffaab3ed8342175c164cfebda8b
Jonathan Diaz <jrd17j@fsu.edu>
2023-03-15 19:41:15 +0000

    Revert "Added current_kernel_time()"
    This reverts commit d4bc4645233055e0551306194230f7c77cd11cac.

--- part2/timer.c
+++ part2/timer.c
@@ -1,22 +1,16 @@
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include <linux/init.h>     /* Needed for __init and __exit */
-#include <linux/time.h>
-
 #define DRIVER_LICENSE "GPL"
 #define DRIVER_AUTHOR "Jonathan Diaz <jrd17j@fsu.edu>"
-#define DRIVER_DESC   "Timer module"
+#define DRIVER_DESC   "Timer Module"
 
-void current_kernel_time()
-{
-    printk(KERN_INFO "In current_kernel_time()");
-    printk(KERN_INFO "Leaving current_kernel_time()");
-}
+static int time_data __initdata = 3;
 
 static int __init my_timer_init(void)
 {
 	printk(KERN_INFO "Starting timer\n");
-    current_kernel_time();
+	printk(KERN_INFO "current time: %d\n", time_data);
 	/* 
 	 * A non 0 return means init_module failed; module can't be loaded. 
 	 */
