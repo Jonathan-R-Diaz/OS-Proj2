d4bc4645233055e0551306194230f7c77cd11cac
Jonathan Diaz <jrd17j@fsu.edu>
2023-03-15 19:37:59 +0000

    Added current_kernel_time()

--- part2/timer.c
+++ part2/timer.c
@@ -1,16 +1,22 @@
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include <linux/init.h>     /* Needed for __init and __exit */
+#include <linux/time.h>
+
 #define DRIVER_LICENSE "GPL"
 #define DRIVER_AUTHOR "Jonathan Diaz <jrd17j@fsu.edu>"
-#define DRIVER_DESC   "Timer Module"
+#define DRIVER_DESC   "Timer module"
 
-static int time_data __initdata = 3;
+void current_kernel_time()
+{
+    printk(KERN_INFO "In current_kernel_time()");
+    printk(KERN_INFO "Leaving current_kernel_time()");
+}
 
 static int __init my_timer_init(void)
 {
 	printk(KERN_INFO "Starting timer\n");
-	printk(KERN_INFO "current time: %d\n", time_data);
+    current_kernel_time();
 	/* 
 	 * A non 0 return means init_module failed; module can't be loaded. 
 	 */
