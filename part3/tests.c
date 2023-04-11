675dae27dd332bafdb0fe626842caf9ffcd30893
Jonathan Diaz <jrd17j@fsu.edu>
2023-04-11 17:35:26 +0000

    Added testcase3

--- part3/tests.c
+++ part3/tests.c
@@ -23,5 +23,31 @@ void testcase2(int num){
         int ret = issue_processor(start, dest, type);
         printk(KERN_DEBUG "Issue (%d, %d, %d) returned %ld\n", start, dest, type, ret);
     }
-    printk(KERN_DEBUG "out of testcase(%d)\n", num);
+    printk(KERN_DEBUG "out of testcase2(%d)\n", num);
+}
+
+void testcase3(int num){
+    // Load elevator
+    printk(KERN_DEBUG "in testcase3\n"); 
+    int type;
+    int start;
+    int dest;
+    int i;
+    for(i = 0; i < num; i+=1)
+    {
+        type = rng(0,2);
+        start = 1;
+        do {
+            dest = 2;
+        } while(dest == start);
+        int ret = issue_processor(start, dest, type);
+        printk(KERN_DEBUG "Issue (%d, %d, %d) returned %ld\n", start, dest, type, ret);
+    }
+    while (!list_empty(&floors[0]) || !list_empty(&elevator_list)) {
+        load_elevator(&floors[0]);
+        FLOOR++;
+        deload_elevator(FLOOR);
+        printk(KERN_DEBUG "out of testcase3(%d)\n", num);
+        FLOOR--;
+    }
 }
